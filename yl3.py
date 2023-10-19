75b8a54122785ab3e49bd161639322074f6e4535
Juhan Heinlaid <98657402+D1ZEASE420@users.noreply.github.com>
2023-10-19 11:43:30 +0300

    Add files via upload

---
+++ yl3.py
@@ -0,0 +1,5 @@
+n = input("Sisesta täisarv: ")
+nn = n+n
+nnn = n+n+n
+Total = int(n) + int(nn) + int(nnn)
+print(n, "+", nn, "+", nnn, "=", Total)
---
+++ yl4.py
@@ -0,0 +1,6 @@
+a = input("Sisesta arv 1: ")
+b = input("Sisesta arv 2: ")
+if int(a) < int(b): 
+    print(a)
+else:
+    print(b)
