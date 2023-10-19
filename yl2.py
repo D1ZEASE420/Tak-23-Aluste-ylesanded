48adee6214cc99ce2214dec5c5c626fdb7d7b033
Juhan Heinlaid <98657402+D1ZEASE420@users.noreply.github.com>
2023-10-19 11:43:53 +0300

    Add files via upload

---
+++ yl2.py
@@ -0,0 +1,10 @@
+import math
+
+radius = float(input ('Sisesta raadius: '))
+
+area = math.pi*radius**2
+circumference = 2*math.pi*radius
+
+
+print("pindala = {}".format(area))
+print("ümbermööt = {}".format(circumference))
