7db703350cecdc4d2621bcda42cdacac98ad79e5
Juhan Heinlaid <98657402+D1ZEASE420@users.noreply.github.com>
2023-10-30 14:15:24 +0200

    Add files via upload

---
+++ yl11.py
@@ -0,0 +1,14 @@
+fruitlist = ["Mango", "Peach", "Cherry"]
+print(fruitlist [0])
+fruitlist.append("kiwi")
+print(fruitlist)
+print(fruitlist [-1])
+fruitlist[1] = "Orange"
+print(fruitlist)
+if "kiwi" in fruitlist:
+    print("kiwi exists in the list")
+print(len(fruitlist))
+fruitlist.pop(3)
+print(fruitlist)
+fruitlist.reverse()
+print(fruitlist)
---
+++ yl12.py
@@ -0,0 +1,7 @@
+pets = ["dog", "cat", "chicken"]
+pet = input("Pet: ")
+print(pet[0])
+pets.append(pet)
+print(pets)
+print(pets[-1][-1])
+
