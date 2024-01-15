7d2b6a6e4b7a742c41ab5a691b7ee97f62531b33
D1ZEASE420 <heinlaidjuhan@gmail.com>
2024-01-15 14:30:27 +0200

    töö

---
+++ mõisted.js
@@ -0,0 +1,34 @@
+//MUUTUJAD
+
+//Esiteks tuleks teha muutuja: var, let, const... var on vana, let on muutuv ja const on muutumatu
+var muutuja;
+// Siis tuleks anda muutujale väärtus
+var muutuja = 7;
+//Väärtuse saame kätte console log'i abil
+console.log(muutuja);
+
+
+//ANDMETÜÜBID
+
+//Andmetüüpe on viis, string, number, array, objekt, boolean
+//Igal andmetüübil on omad omadused erinemaks teistest andmetüüpidest, stringil "jutumärgid", numbril mitte midagi,
+//array'l [kantsulud], objektil {nibudega sulud} kuid boolean on lihtsalt true/false ja mingit otsest märget nagu on stringil tal ei ole.
+"string", number4, [array], {objekt}, true/false
+
+
+//Conditionals
+
+//Conditionalid on viis panemaks oma kood tegema midagi kui miski muu ebaõnnestub
+//Conditionalid on if, else, elif
+if, else, elif
+
+
+//Loops
+
+//Functions
+//Functionitega saab erinevaid asju 
+function tereTulemast() {
+  console.log("Tere tulemast!")
+
+  }
+
